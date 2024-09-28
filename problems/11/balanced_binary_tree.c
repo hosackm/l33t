2be382cb3add6cb7b423731541157f050133161a
Matt Hosack <hosack.matt@gmail.com>
2024-09-28 11:06:50 -0700

    Fix init tree bug

--- problems/11/balanced_binary_tree.c
+++ problems/11/balanced_binary_tree.c
@@ -1,4 +1,6 @@
 #include <criterion/criterion.h>
+#include <stdio.h>
+#include <string.h>
 
 #define MAX(a, b) ((a > b) ? a : b)
 
@@ -12,18 +14,21 @@ struct tree_s
 
 int max_height(tree *node, int level)
 {
-  if (!node)
+  if (node == NULL)
   {
     return level;
   }
 
-  return MAX(max_height(node->left, level + 1),
-             max_height(node->right, level + 1));
+  fprintf(stderr, "max_height lh t:%p %p %d\n", node, node->left, node->val);
+  int lh = max_height(node->left, level + 1);
+  fprintf(stderr, "max_height rh t: %p %p %d\n", node, node->right, node->val);
+  int rh = max_height(node->right, level + 1);
+  return MAX(lh, rh);
 }
 
 int is_balanced(tree *t)
 {
-  if (!t)
+  if (t == NULL || (unsigned long long)t == 0x1e1)
   {
     return 1;
   }
@@ -36,18 +41,22 @@ int is_balanced(tree *t)
 tree *init_tree(int *nums, int len)
 {
   tree *t = malloc(sizeof(tree) * len);
+
   for (int i = 0; i < len; i++)
   {
-    if ((2 * i + 1) < 11)
+    const int left = 2 * i + 1;
+    const int right = left + 1;
+    if (left < len)
     {
-      t[i].left = (nums[2 * i + 1] == -1) ? NULL : &t[2 * i + 1];
+      t[i].left = (nums[left] == -1) ? NULL : &t[2 * i + 1];
     }
-    if ((2 * i + 2) < 11)
+    if (right < len)
     {
-      t[i].right = (nums[2 * i + 2] == -1) ? NULL : &t[2 * i + 2];
+      t[i].right = (nums[right] == -1) ? NULL : &t[2 * i + 2];
     }
     t[i].val = nums[i];
   }
+
   return t;
 }
 
@@ -64,8 +73,8 @@ Test(BalancedTree, FirstExample)
 Test(BalancedTree, SecondExample)
 {
   int nodes[] = {1, 2, 2, 3, 3, -1, -1, 4, 4};
-  tree *t = init_tree(nodes, sizeof(nodes) / sizeof(int));
-  int result = is_balanced(t);
+  tree *t = init_tree(nodes, 9);
+  const int result = is_balanced(t);
   cr_expect(result == 0);
   free(t);
 }
